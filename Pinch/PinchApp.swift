42b3cdd141f8930fc185ac3c80c38dde93754683
Gnanapriya <gnanapriya.cr@gmail.com>
2023-02-07 00:09:53 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by gnanapriya.chandran on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
